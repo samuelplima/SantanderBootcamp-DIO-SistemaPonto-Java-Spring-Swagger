b70fe5719efe4e79b2d3c47cba25ff77c0c4b665
Samuel Lima <80234488+smk-1991@users.noreply.github.com>
2021-09-03 13:32:49 -0300

    Add files via upload

---
+++ SistemaDePonto/src/main/java/com/dio/project/repository/JornadaRepository.java
@@ -0,0 +1,9 @@
+package com.dio.project.repository;
+
+import com.dio.project.model.JornadaTrabalho;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JornadaRepository extends JpaRepository<JornadaTrabalho, Long> {
+}
